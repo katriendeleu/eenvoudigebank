fc00a1ee4631617015f4b64dad7e41d27c6bd995
Katrien Deleu <katrien.deleu@vives.be>
2018-01-11 12:41:39 +0100

    eerste lege versie

---
+++ EenvoudigeBank_BitBucket/src/eenvoudigebank_bitbucket/EenvoudigeBank_BitBucket.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eenvoudigebank_bitbucket;
+
+/**
+ *
+ * @author Katrien.Deleu
+ */
+public class EenvoudigeBank_BitBucket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("DIT IS MIJN VERSIE");
+    }
+    
+}
